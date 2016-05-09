dfeea5e9c7ad9c3b6ce39dedeac887d403eb478c
sushantraj22 <sushcool79@gmail.com>
2016-05-09 17:47:59 -0400

    Update CreateModifyTxt.java

--- CreateModifyTxt.java
+++ CreateModifyTxt.java
@@ -19,7 +19,7 @@ public class CreateModifyTxt{
 		try (Writer writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(name + ".txt"), StandardCharsets.UTF_8))) {
 			System.out.println("Topic?");
 			String topic = inputword();
-		    writer.write("${bold}" + topic + "${normal}");
+		    writer.write("${bold}\t" + topic + "${normal}");
 		    System.out.println("begin writing bullet points, press enter to start a new bullet");
 		    String bullet = inputWords();
 		    writer.write("\n");
