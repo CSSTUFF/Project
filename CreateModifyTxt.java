c74ac99a4c8b12acd0e373b45dc606f17a4aa756
sushantraj22 <sushcool79@gmail.com>
2016-05-09 21:09:44 -0400

    Update CreateModifyTxt.java

--- CreateModifyTxt.java
+++ CreateModifyTxt.java
@@ -1,11 +1,8 @@
 
 import java.util.*;
-import java.io.PrintWriter;
 import java.util.Date;
 import java.io.BufferedWriter;
-import java.io.File;
 import java.io.FileOutputStream;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
 import java.io.Writer;
@@ -13,7 +10,7 @@ import java.nio.charset.StandardCharsets;
 public class CreateModifyTxt{
 
 	public static void main(String[] args) throws IOException {
-		
+		List<KeyTermsOBJ> terms = new ArrayList<KeyTermsOBJ>();
 		System.out.println("File Name?");
 		String name = inputword();
 		try (Writer writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(name + ".txt"), StandardCharsets.UTF_8))) {
@@ -28,7 +25,28 @@ public class CreateModifyTxt{
 		    	 if(!bullet.equals("end doc"))
 		    	 writer.write("-" + bullet + "\n");
 		    }
-		} catch (IOException ex) {
+		 System.out.println("Key Terms?(yes or no)");
+		 String ans = inputword();
+		 String term = "";
+		 if(ans.toLowerCase().charAt(0)=='y'){
+			  while(!term.equals("end doc")){
+				  	 System.out.println("term?");
+			    	 term = inputword();
+			    	 if(!term.equals("end doc")){
+			    	 System.out.println("term?");
+			    	 String def = inputWords();
+			    	 KeyTermsOBJ k = new KeyTermsOBJ(term, def);
+			    	 terms.add(k);
+			    	}
+			    } 
+			  writer.write("\n");
+			  for(KeyTermsOBJ k: terms){
+				  writer.write(k.getTerm() + " --> " + k.getDef() + "\n");
+			  }
+		 }
+		 
+		} 
+		catch (IOException ex) {
 		    // handle me
 		} 
 
