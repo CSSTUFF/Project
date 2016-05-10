8813e1d644c15dbccb21688827e404e345e299db
sushantraj22 <sushcool79@gmail.com>
2016-05-09 21:10:12 -0400

    Add files via upload

---
+++ KeyTermsOBJ.java
@@ -0,0 +1,11 @@
+public class KeyTermsOBJ {
+private String term;
+private String def;
+public KeyTermsOBJ(String t, String d){
+	term = t;
+	def = d;
+}
+public String getDef(){
+	return def;
+}
+}
