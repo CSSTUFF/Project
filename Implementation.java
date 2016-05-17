4d447ed2e63538390652d66289f450c6865c48e8
pwadas <pwadas2018@student.amsacs.org>
2016-05-17 12:04:57 -0400

    Update Implementation.java

--- Implementation.java
+++ Implementation.java
@@ -1,61 +1,71 @@
-
-public class Implementation extends Subjects{
+import java.util.*;
+public class Implementation{
 
 	public static void main(String[] args) {
-		keyTerms();
-
+		Scanner con= new Scanner(System.in);
+		System.out.print("Would you like to see the concepts or the keyterms of this topic?");
+		String choice= con.nextLine();
+		if(choice.equals("concepts")==true){
+			keyConcepts();
+		}
+		else{
+			keyTerms();
+		}	}
+	public static void keyConcepts(){
+		System.out.println("Concepts: ");
+		System.out.println("~ Programming constructs");
+		System.out.println("~ Declaration");
+		System.out.println("~ Printing outputs");
+		System.out.println("~ Expressions");
 	}
 	public static void keyTerms(){
+		System.out.println("Terms:");
 		System.out.print("~ Object-oriented:  ");
 		System.out.println("a programing language that uses methodology enabling"
 				+ " a system to be modeled as a set of objects that can be"
 				+ " controlled/manipulated in a mudular manner");
-		System.out.print("Encapsulation:  ");
+		System.out.print("~ Encapsulation:  ");
 		System.out.println("the technique of making the fields in a class"
 				+ " private and providing access to the fields via public"
 				+ " methods. It can not be accessed by other classes");
-		System.out.print("Information Hiding:  ");
-		System.out.println("mechanism for restricting access to some of the"
+		System.out.print("~ Information Hiding:  mechanism for restricting access to some of the"
 				+ " object's components");
-		System.out.print("Primitive Types:  one of the two Java programming "
+		System.out.println("~ Primitive Types:  one of the two Java programming "
 				+ "language types; ");
-		System.out.println("");
-		System.out.print("Reference Types:  one of the two Java programming "
+		System.out.println("~ Reference Types:  one of the two Java programming "
 				+ "language types; class types, interface types, and array types");
-		System.out.println("");
-		System.out.print("Declaration:  ");
-		System.out.println("");
-		System.out.print("Constants:  ");
-		System.out.println("");
-		System.out.print("Constants:  ");
-		System.out.println("");
-		System.out.print("Variables:  ");
-		System.out.println("");
-		System.out.print("Methods:  ");
-		System.out.println("");
-		System.out.print("Parameters:  ");
-		System.out.println("");
-		System.out.print("Classes:  ");
-		System.out.println("");
-		System.out.print("Interfaces:  ");
-		System.out.println("");
-		System.out.print("Method Call:"); 
-		System.out.println("");
-		System.out.print("Sequential Execution:  ");
-		System.out.println("");
-		System.out.print("Conditional Execution:  ");
-		System.out.println("");
-		System.out.print("Iteration:  ");
-		System.out.println("");
-		System.out.print("Recursion:  ");
-		System.out.println("");
-		System.out.print("Boolean Expressions:  ");
-		System.out.println("");
-		System.out.print("String Expressions:  ");
-		System.out.println("");
-		System.out.print("Numeric Expressions:  ");
-		System.out.println("");
-		System.out.print("Expression Evaluation:  ");
-		System.out.println("");
+		System.out.println("~ Declaration:  variable declaration is defining "
+				+ "the variable type, method declarations are the method's"
+				+ " return type, name, a pair of parentheses, (), and a body"
+				+ " between braces, {}");
+		System.out.println("~ Constants:  a variable whose value cannot change"
+				+ " once it has been assigned");
+		System.out.println("~ Variables:  a container that holds values that are"
+				+ " used in a Java program thatmust be declared to use a data type");
+		System.out.println("~ Methods:  a set of code which is referred to by name"
+				+ " and can be invoked at any point in a program with the method's"
+				+ " name");
+		System.out.println("~ Parameters:  variables that are listed as part of"
+				+ " a method declaration, that must have a unique name and a "
+				+ "defined data type");
+		System.out.println("~ Classes:  a blueprint or a template for making"
+				+ " different objects which defines its properties and behaviors."
+				+ " They can have feilds and methods to describe the behavior"
+				+ " of an object");
+		System.out.println("~ Interfaces:  a body of code that can have only abstract"
+				+ "methods and constants. They are implemented by classes");
+		System.out.println("~ Method Call:  the invocation of a method"); 
+		System.out.println("~ Sequential Execution:  when the instructions are "
+				+ "executed in the same order as the output, without skipping"
+				+ " or repeating any instuctions from the code");
+		System.out.println("~ Conditional Execution:  if(true) else(false) statement");
+		System.out.println("~ Iteration:  the repetition of code; "
+				+ "e.i. while or for loops");
+		System.out.println("~ Recursion:  a method that calls itself");
+		System.out.println("~ Boolean Expressions:  a true or false statement");
+		System.out.println("~ String Expressions:  an expression of letters");
+		System.out.println("~ Numeric Expressions:  an expression using numbers");
+		System.out.println("~ Expression Evaluation:  a specific expression in italics");
 	}
+	public Object[] subjects;
 }
