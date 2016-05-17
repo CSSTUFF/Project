c8299a3778abed5a3b47b734db5527f25831e17c
pwadas <pwadas2018@student.amsacs.org>
2016-05-17 12:11:23 -0400

    Update Subjects.java

--- Subjects.java
+++ Subjects.java
@@ -1,11 +1,35 @@
+import java.util.Scanner;
 
-public class Subjects {
-	private String[] subjects={"Implementation","Data Structures",
-			"Operations and Algorithms","Program Analysis"}; 
-	public Subjects(){
-		
+
+public class Subjects{
+	public static void main(String[] args) {
+		String[] subjects={"implementation","data structures",
+			"operations and algorithms","program analysis"};
+		Implementation Implementation = new Implementation();
+		Data_Structures Data_Structures = new Data_Structures();
+		Operations_and_Algorithms Operations_and_Algorithms= new Operations_and_Algorithms();
+		Program_Analysis Program_Analysis= new Program_Analysis();
+		Scanner con= new Scanner(System.in);
+		System.out.print("Would you like to look at notes or play a review game?"+
+				"(type notes or review)");
+		String selection=con.next();
+		if(selection.toLowerCase().contains("note")==true){
+			System.out.print("Which topic would you like to study? "
+				+ "\nImplementation, Data Structures, "
+				+ "Operations and Algorithms, or Program Analysis");
+			String topic=con.next();
+			chooser(topic,subjects);
+		}
+		else{
+			/**review class**/
+		}
 	}
-	public Subjects(String selection){
-		
+	public static void chooser(String selection, String[] subjects){
+		selection=selection.toLowerCase();
+		for(int i=0;i<subjects.length;i++){
+			if(subjects[i].contains(selection)){
+				System.out.println();
+			}
+		}
 	}
 }
