d9c16aac15df5988d31f74947d75188a6ad4ec4c
pwadas <pwadas2018@student.amsacs.org>
2016-06-01 10:35:26 -0400

    Update Operations_and_Algorithms.java

--- Operations_and_Algorithms.java
+++ Operations_and_Algorithms.java
@@ -10,7 +10,37 @@ public class Operations_and_Algorithms {
 		else{
 			keyTerms();
 		}
+	}import java.util.*;
+public class Operations_and_Algorithms {
+	//the "teacher made" key concepts of this java topic
+	public static void keyConcepts(){
+		System.out.println("Concepts: ");
+		System.out.println("~ Operations on data structures");
+		System.out.println("~ Analysis of program efficiency");
 	}
+	//the "teacher made" key terms of this java topic
+	public static void keyTerms(){
+		System.out.println("Terms:");
+		System.out.println("~ Traversals:  going though each element in an sequence");
+		System.out.println("~ Insertions:  a way to sort an array by traversing "
+				+ "once");
+		System.out.println("~ Searching:  finding an item in the array that meets"
+				+ " some specified criterion");
+		System.out.println("~ Sequential:  it is accessed by a single thread; "
+				+ "a single thread can only do code in a specific order");
+		System.out.println("~ Binary:  one of the notations recognized"
+				+ " by old Java versions (octal, decimal, or hexadecimal)");
+		System.out.println("~ Sorting:  to put data in order; either"
+				+ " numerically or alphabetically");
+		System.out.println("~ Insertion: sortting data by only going through "
+				+ "one traversal");
+		System.out.println("~ Selection:  sorting algorithm, specifically"
+				+ " an in-place comparison sort");
+		System.out.println("~ Mergesort:  a general-purpose,"
+				+ " comparison-based sorting algorithm");
+	}
+	public Object[] subjects;
+}
 	public static void keyConcepts(){
 		System.out.println("Concepts: ");
 		System.out.println("~ Operations on data structures");
