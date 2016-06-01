bfa82fc836d247701f79c2df0139a5a9e698ad87
pwadas <pwadas2018@student.amsacs.org>
2016-06-01 10:39:25 -0400

    Update Implementation.java

--- Implementation.java
+++ Implementation.java
@@ -1,38 +1,62 @@
 import java.util.*;
-public class Program_Analysis {
+public class Implementation{
 	//the "teacher made" key concepts of this java topic
 	public void keyConcepts(){
 		System.out.println("Concepts: ");
-		System.out.println("~ Examining and testing programs to determine"
-				+ " if they correctly meet their specifications");
-		System.out.println("~ Analysis of programs or algorithms to"
-				+ " understand their time and space requirements when"
-				+ " applied to different data sets");
-		System.out.println("~ Error identification and correction");
+		System.out.println("~ Programming constructs");
+		System.out.println("~ Declaration");
+		System.out.println("~ Printing outputs");
+		System.out.println("~ Expressions");
 	}
 	//the "teacher made" key terms of this java topic
 	public void keyTerms(){
 		System.out.println("Terms:");
-		System.out.println("Unit testing:   a software testing method"
-				+ " by which individual units of source code");
-		System.out.println("Integration testing:  a software development"
-				+ " process which program units are combined and tested"
-				+ " as groups in multiple ways");
-		System.out.println("Debugging:  the process of locating and "
-				+ "fixing or bypassing errors(bugs) in computer a "
-				+ "program code or the engineering of a hardware device");
-		System.out.println("Compile-time:   any type of error that "
-				+ "prevent a java program compile like a syntax error, "
-				+ "a class not found, a bad file name for the defined class,"
-				+ " a possible loss of precision when you are mixing "
-				+ "different java data types and so on");
-		System.out.println("Run-time:  an error which happens, while the"
-				+ " program is running");
-		System.out.println("Runtime exceptions:  unchecked exceptions");
-		System.out.println("Preconditions:  what must always true before"
-				+ " the execution of a section of code is outputted");
-		System.out.println("postconditions:  what must be true after the"
-				+ " method is complete");
+		System.out.print("~ Object-oriented:  ");
+		System.out.println("a programing language that uses methodology enabling"
+				+ " a system to be modeled as a set of objects that can be"
+				+ " controlled/manipulated in a mudular manner");
+		System.out.print("~ Encapsulation:  ");
+		System.out.println("the technique of making the fields in a class"
+				+ " private and providing access to the fields via public"
+				+ " methods. It can not be accessed by other classes");
+		System.out.print("~ Information Hiding:  mechanism for restricting access to some of the"
+				+ " object's components");
+		System.out.println("~ Primitive Types:  one of the two Java programming "
+				+ "language types; ");
+		System.out.println("~ Reference Types:  one of the two Java programming "
+				+ "language types; class types, interface types, and array types");
+		System.out.println("~ Declaration:  variable declaration is defining "
+				+ "the variable type, method declarations are the method's"
+				+ " return type, name, a pair of parentheses, (), and a body"
+				+ " between braces, {}");
+		System.out.println("~ Constants:  a variable whose value cannot change"
+				+ " once it has been assigned");
+		System.out.println("~ Variables:  a container that holds values that are"
+				+ " used in a Java program thatmust be declared to use a data type");
+		System.out.println("~ Methods:  a set of code which is referred to by name"
+				+ " and can be invoked at any point in a program with the method's"
+				+ " name");
+		System.out.println("~ Parameters:  variables that are listed as part of"
+				+ " a method declaration, that must have a unique name and a "
+				+ "defined data type");
+		System.out.println("~ Classes:  a blueprint or a template for making"
+				+ " different objects which defines its properties and behaviors."
+				+ " They can have feilds and methods to describe the behavior"
+				+ " of an object");
+		System.out.println("~ Interfaces:  a body of code that can have only abstract"
+				+ "methods and constants. They are implemented by classes");
+		System.out.println("~ Method Call:  the invocation of a method"); 
+		System.out.println("~ Sequential Execution:  when the instructions are "
+				+ "executed in the same order as the output, without skipping"
+				+ " or repeating any instuctions from the code");
+		System.out.println("~ Conditional Execution:  if(true) else(false) statement");
+		System.out.println("~ Iteration:  the repetition of code; "
+				+ "e.i. while or for loops");
+		System.out.println("~ Recursion:  a method that calls itself");
+		System.out.println("~ Boolean Expressions:  a true or false statement");
+		System.out.println("~ String Expressions:  an expression of letters");
+		System.out.println("~ Numeric Expressions:  an expression using numbers");
+		System.out.println("~ Expression Evaluation:  a specific expression in italics");
 	}
-	public Object[] subjects;
+
 }
